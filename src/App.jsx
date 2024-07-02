a5727c608c061b6677b6c52257414af88b90107f
mariangel77 <mariangelwork1@gmail.com>
2024-07-02 18:59:57 -0300

    preentrega1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { useState } from "react"
+import NavBar from "./components/NavBar/NavBar"
+
+
+function App() {
+  const [cartCount, setCartCount] = useState(5)
+
+  return (
+    <>
+      <NavBar cartCount={cartCount}/>
+    </>
+  )
+}
+
+export default App
